cbceee97e43b5b8c04b5b2c321fb909db6e22b63
mdiyar <mdiyar@bitdefender.com>
2023-07-31 22:04:22 +0300

    Added precision, recall and f1 functionality

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, jsonify
-from surprise import  Dataset, Reader
+from surprise import Dataset, Reader
 from surprise.model_selection import train_test_split
 from surprise.accuracy import rmse, mae
 import plotly.graph_objects as go
@@ -11,71 +11,94 @@ import eval_functions as eval_func
 app = Flask(__name__)
 
 # Load the dataset
-data = Dataset.load_builtin('ml-100k')
+data = Dataset.load_builtin("ml-100k")
 reader = Reader(rating_scale=(1, 5))
 
 train_data, test_data = train_test_split(data, train_size=0.8)
 print(len(test_data))
-legal_models = ['SVD', 'KNN', 'Baseline', 'SlopeOne', 'CoClustering']
+legal_models = ["SVD", "KNN", "Baseline", "SlopeOne", "CoClustering"]
 
-@app.route('/')
+
+@app.route("/")
 def show_main():
-    ...
-    
-@app.route('/mae_rmse')
+    return render_template("main.html")
+
+
+@app.route("/mae_rmse")
 def show_mae_rmse_graph():
-    potential_models = request.args.getlist('model')
-    
+    potential_models = request.args.getlist("model")
+
     selected_models = set(potential_models) & set(legal_models)
-    
+
     if not selected_models:
         selected_models = legal_models
-        
-    mae_to_plot, rmse_to_plot, time_to_plot = utils.get_plots(selected_models, utils.get_mae_rmse_score_bars)
+
+    mae_to_plot, rmse_to_plot, time_to_plot = utils.get_plots(
+        selected_models, utils.get_mae_rmse_score_bars
+    )
 
     fig_mae = go.Figure(data=mae_to_plot)
-    fig_mae.update_layout(title="Eroarea medie absoluta (MAE)", barmode='group')
+    fig_mae.update_layout(title="Eroarea medie absoluta (MAE)", barmode="group")
 
     fig_rmse = go.Figure(data=rmse_to_plot)
-    fig_rmse.update_layout(title="Radacina erorii mediei patratice (RMSE)", barmode='group')
-    
+    fig_rmse.update_layout(
+        title="Radacina erorii mediei patratice (RMSE)", barmode="group"
+    )
+
     fig_time = go.Figure(data=time_to_plot)
-    fig_time.update_layout(title="Timpul de antrenare si de predictie a modelelor", barmode='group', yaxis=dict(range=(0, max(time_to_plot, key=lambda time: time['y'])['y'][0] + 0.2)))
+    fig_time.update_layout(
+        title="Timpul de antrenare si de predictie a modelelor",
+        barmode="group",
+        yaxis=dict(
+            range=(0, max(time_to_plot, key=lambda time: time["y"])["y"][0] + 0.2)
+        ),
+    )
 
     return render_template(
-        'mae_rmse.html',
+        "mae_rmse.html",
         mae_plot=fig_mae.to_html(),
         rmse_plot=fig_rmse.to_html(),
-        time_plot=fig_time.to_html()
-        )
+        time_plot=fig_time.to_html(),
+    )
 
-@app.route('/precision_recall_f1')
+
+@app.route("/precision_recall_f1")
 def show_precision_f1_recall():
-    potential_models = request.args.getlist('model')
+    potential_models = request.args.getlist("model")
     selected_models = potential_models and legal_models
-    
+
     if not selected_models:
         selected_models = legal_models
-    
-    precision_to_plot, recall_to_plot, f1_to_plot = utils.get_plots(selected_models, utils.get_precision_recall_f1_score_bars)
-    
+
+    k_value = request.args.get("k")
+
+    if not k_value:
+        k_value = 10
+    else:
+        k_value = int(k_value)
+
+    precision_to_plot, recall_to_plot, f1_to_plot = utils.get_plots(
+        selected_models, utils.get_precision_recall_f1_score_bars, k_value
+    )
+
     fig_precision = go.Figure(data=precision_to_plot)
-    fig_precision.update_layout(title='Precision', barmode='group')
-    
+    fig_precision.update_layout(title="Precision", barmode="group")
+
     fig_recall = go.Figure(data=recall_to_plot)
-    fig_recall.update_layout(title='Recall', barmode='group')
-    
+    fig_recall.update_layout(title="Recall", barmode="group")
+
     fig_f1 = go.Figure(data=f1_to_plot)
-    fig_f1.update_layout(title='F1', barmode='group')
-    
+    fig_f1.update_layout(title="F1", barmode="group")
+
     return render_template(
-        'precision_recall_f1.html',
+        "precision_recall_f1.html",
         precision_plot=fig_precision.to_html(),
         recall_plot=fig_recall.to_html(),
-        f1_plot=fig_f1.to_html()
-        )
-    
-@app.route('/api/v1/mae_rmse/<model_name>', methods=['GET'])
+        f1_plot=fig_f1.to_html(),
+    )
+
+
+@app.route("/api/v1/mae_rmse/<model_name>", methods=["GET"])
 def get_mae_rmse_model(model_name):
     model = utils.get_model_instance(model_name)
     start_time = time.time()
@@ -86,22 +109,25 @@ def get_mae_rmse_model(model_name):
     mae_score = mae(predictions, verbose=False)
     rmse_score = rmse(predictions, verbose=False)
 
-    return jsonify(rmse_score, mae_score, end_time-start_time)
+    return jsonify(rmse_score, mae_score, end_time - start_time)
 
-@app.route('/api/v1/precision_recall_f1/<model_name>/<k>', methods=['GET'])
+
+@app.route("/api/v1/precision_recall_f1/<model_name>/<k>", methods=["GET"])
 def get_recall_precision_f1_model_k(model_name, k):
-    
     model = utils.get_model_instance(model_name)
-    
+
     model.fit(train_data)
-    
+
     predictions = model.test(test_data)
-    
+
     k = int(k)
-    
-    precision_score, recall_score, f1_score = eval_func.precision_recall_f1(test_data, predictions, k)
-    
+
+    precision_score, recall_score, f1_score = eval_func.precision_recall_f1(
+        test_data, predictions, k
+    )
+
     return jsonify(precision_score, recall_score, f1_score)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(port=8000, debug=True)
--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import requests
 import plotly.graph_objects as go
 from surprise import SVD, KNNBasic, BaselineOnly, SlopeOne, CoClustering
 
+
 def get_model_instance(model_name):
     model_factories = {
         "SVD": lambda: SVD(),
@@ -13,39 +14,116 @@ def get_model_instance(model_name):
 
     return model_factories[model_name]()
 
+
 def get_plots(selected_models, function, k=0):
     plots = []
-    
-    for model_name in selected_models:
-        for i, score_bar in enumerate(function(model_name)):
-            try:
-                if not plots[i]:
-                    plots[i] = []
-            except IndexError:
-                plots.append([])
-            plots[i].append(score_bar)
+
+    if k:
+        for model_name in selected_models:
+            for i, score_bar in enumerate(function(model_name, k)):
+                try:
+                    if not plots[i]:
+                        plots[i] = []
+                except IndexError:
+                    plots.append([])
+                plots[i].append(score_bar)
+    else:
+        for model_name in selected_models:
+            for i, score_bar in enumerate(function(model_name)):
+                try:
+                    if not plots[i]:
+                        plots[i] = []
+                except IndexError:
+                    plots.append([])
+                plots[i].append(score_bar)
 
     return plots
 
+
 def get_mae_rmse_score_bars(model_name):
     bar_width = 0.1
-    rmse_score, mae_score, elapsed_time = requests.get(f"http://127.0.0.1:8000/api/v1/mae_rmse/{model_name}").json()
+    rmse_score, mae_score, elapsed_time = requests.get(
+        f"http://127.0.0.1:8000/api/v1/mae_rmse/{model_name}"
+    ).json()
 
-    mae_bar = go.Bar(name=f'{model_name}', x=['MAE'], y=[mae_score], width=bar_width, text=[f'{model_name} <br> MAE: {round(mae_score, 6)}'], textposition='inside', insidetextanchor='middle', hoverinfo='text', hovertext=f'Model: {model_name} <br>MAE: {mae_score}')
-    rmse_bar = go.Bar(name=f'{model_name}', x=['RMSE'], y=[rmse_score], width=bar_width, text=[f'{model_name} <br> RMSE: {round(rmse_score, 6)}'], textposition='inside', insidetextanchor='middle', hoverinfo='text', hovertext=f'Model: {model_name} <br>RMSE: {rmse_score}')
-    time_bar = go.Bar(name=f'{model_name}', x=['Time'], y=[elapsed_time], width=bar_width, text=[f'{model_name} <br> Time: {round(elapsed_time, 6)}s'], textposition='outside', hoverinfo='text', hovertext=f'Model: {model_name} <br>Time: {round(elapsed_time, 6)}s')
-    
+    mae_bar = go.Bar(
+        name=f"{model_name}",
+        x=["MAE"],
+        y=[mae_score],
+        width=bar_width,
+        text=[f"{model_name} <br> MAE: {round(mae_score, 6)}"],
+        textposition="inside",
+        insidetextanchor="middle",
+        hoverinfo="text",
+        hovertext=f"Model: {model_name} <br>MAE: {mae_score}",
+    )
+    rmse_bar = go.Bar(
+        name=f"{model_name}",
+        x=["RMSE"],
+        y=[rmse_score],
+        width=bar_width,
+        text=[f"{model_name} <br> RMSE: {round(rmse_score, 6)}"],
+        textposition="inside",
+        insidetextanchor="middle",
+        hoverinfo="text",
+        hovertext=f"Model: {model_name} <br>RMSE: {rmse_score}",
+    )
+    time_bar = go.Bar(
+        name=f"{model_name}",
+        x=["Time"],
+        y=[elapsed_time],
+        width=bar_width,
+        text=[f"{model_name} <br> Time: {round(elapsed_time, 6)}s"],
+        textposition="outside",
+        hoverinfo="text",
+        hovertext=f"Model: {model_name} <br>Time: {round(elapsed_time, 6)}s",
+    )
+
     return mae_bar, rmse_bar, time_bar
 
-def get_precision_recall_f1_score_bars(model_name):
+
+def get_precision_recall_f1_score_bars(model_name, k):
     bar_width = 0.1
-    precision_score, recall_score, f1_score = requests.get(f"http://127.0.0.1:8000/api/v1/precision_recall_f1/{model_name}").json()
-    
-    precision_bar = go.Bar(name=f'{model_name}', x=['Precision'], y=[precision_score], width=bar_width, text=[f'{model_name} <br> Precision: {round(precision_score, 6)}'], textposition='inside', insidetextanchor='middle', hoverinfo='text', hovertext=f'Model: {model_name} <br>Precision: {precision_score}')
-    recall_bar = go.Bar(name=f'{model_name}', x=['Recall'], y=[recall_score], width=bar_width, text=[f'{model_name} <br> Recall: {round(recall_score, 6)}'], textposition='inside', insidetextanchor='middle', hoverinfo='text', hovertext=f'Model: {model_name} <br>Recall: {recall_score}')
-    f1_bar = go.Bar(name=f'{model_name}', x=['F1'], y=[f1_score], width=bar_width, text=[f'{model_name} <br> F1: {round(f1_score, 6)}s'], textposition='inside',insidetextanchor='middle', hoverinfo='text', hovertext=f'Model: {model_name} <br>F1: {round(f1_score, 6)}s')
-    
+    precision_score, recall_score, f1_score = requests.get(
+        f"http://127.0.0.1:8000/api/v1/precision_recall_f1/{model_name}/{k}"
+    ).json()
+
+    precision_bar = go.Bar(
+        name=f"{model_name}",
+        x=["Precision"],
+        y=[precision_score],
+        width=bar_width,
+        text=[f"{model_name} <br> Precision: {round(precision_score, 6)}"],
+        textposition="inside",
+        insidetextanchor="middle",
+        hoverinfo="text",
+        hovertext=f"Model: {model_name} <br>Precision: {precision_score}",
+    )
+    recall_bar = go.Bar(
+        name=f"{model_name}",
+        x=["Recall"],
+        y=[recall_score],
+        width=bar_width,
+        text=[f"{model_name} <br> Recall: {round(recall_score, 6)}"],
+        textposition="inside",
+        insidetextanchor="middle",
+        hoverinfo="text",
+        hovertext=f"Model: {model_name} <br>Recall: {recall_score}",
+    )
+    f1_bar = go.Bar(
+        name=f"{model_name}",
+        x=["F1"],
+        y=[f1_score],
+        width=bar_width,
+        text=[f"{model_name} <br> F1: {round(f1_score, 6)}s"],
+        textposition="inside",
+        insidetextanchor="middle",
+        hoverinfo="text",
+        hovertext=f"Model: {model_name} <br>F1: {round(f1_score, 6)}s",
+    )
+
     return precision_bar, recall_bar, f1_bar
+
 
 def get_top_k_percent(sorted_list, k):
     if not (0 <= k <= 100):
