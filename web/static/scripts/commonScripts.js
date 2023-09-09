6a910da492be4f16a06ac2844ec1ad29ea1ae264
mdiyar <mdiyar@bitdefender.com>
2023-09-10 00:03:35 +0300

    input validation and scripts changed - added a general function for input validation - moved the common scripts to the commonScripts.js file - set required fields

--- web/static/scripts/commonScripts.js
+++ web/static/scripts/commonScripts.js
@@ -2,3 +2,56 @@ function setActiveMenu(activeMenu) {
     const menuOption = document.querySelector(`li.nav-item a.nav-link[href="/${activeMenu}"]`);
     menuOption.classList.add("active");
 }
+
+const isFormValid = async () => {
+    const form = document.getElementById('evaluationForm');
+    const formElements = form.elements;
+    const evaluateButton = document.getElementById('evaluate_button');
+    let valid = true;
+    let checkedBox = false;
+
+    for (let i = 0; i < formElements.length; i++) {
+        const element = formElements[i];
+
+        if (element.name === "model"  && element.checked) {
+          checkedBox = true;
+        }
+
+        if (!element.checkValidity()) {
+            valid = false;
+            break;
+        }
+
+        if (element.value === "") {
+            valid = false;
+            break;
+        }
+    }
+
+    if (valid && checkedBox) {
+      evaluateButton.disabled = false
+    }
+    else {
+        evaluateButton.disabled = true
+    }
+  };
+
+const selectAll = async () => {
+    document.getElementById("selectAll").addEventListener("change", (event) => {
+        const checkboxes = document.querySelectorAll(
+            'input[type="checkbox"][name="model"]'
+        );
+        for (let i = 0; i < checkboxes.length; i++) {
+            checkboxes[i].checked = event.target.checked;
+        }
+    });
+}
+
+const evaluateButton = async () => {
+    document
+    .getElementById("evaluate_button")
+    .addEventListener("click", async (event) => {
+      event.preventDefault();
+      await plotModels();
+    });
+}
--- web/static/scripts/toggleComparisonInputs.js
+++ web/static/scripts/toggleComparisonInputs.js
@@ -7,3 +7,14 @@ function displayK() {
     const kInput = document.getElementById("comparison_inputs");
     kInput.style.display = "block";
 }
+
+const toggleK = async() => {
+    document.getElementById('comparison_radio_group').addEventListener('change', (event) => {
+        if( event.target.id === 'precision_recall_f1') {
+            displayK();
+        }
+        else {
+            hideK();
+        }
+    });
+}
