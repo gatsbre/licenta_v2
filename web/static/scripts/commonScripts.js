7982b81c796db0f041517607382be46e3f1521e6
mdiyar <mdiyar@bitdefender.com>
2023-09-10 00:41:53 +0300

    formatted code

--- web/static/scripts/commonScripts.js
+++ web/static/scripts/commonScripts.js
@@ -1,57 +1,58 @@
 function setActiveMenu(activeMenu) {
-    const menuOption = document.querySelector(`li.nav-item a.nav-link[href="/${activeMenu}"]`);
-    menuOption.classList.add("active");
+  const menuOption = document.querySelector(
+    `li.nav-item a.nav-link[href="/${activeMenu}"]`
+  );
+  menuOption.classList.add("active");
 }
 
 const isFormValid = async () => {
-    const form = document.getElementById('evaluationForm');
-    const formElements = form.elements;
-    const evaluateButton = document.getElementById('evaluate_button');
-    let valid = true;
-    let checkedBox = false;
+  const form = document.getElementById("evaluationForm");
+  const formElements = form.elements;
+  const evaluateButton = document.getElementById("evaluate_button");
+  let valid = true;
+  let checkedBox = false;
 
-    for (let i = 0; i < formElements.length; i++) {
-        const element = formElements[i];
+  for (let i = 0; i < formElements.length; i++) {
+    const element = formElements[i];
 
-        if (element.name === "model"  && element.checked) {
-          checkedBox = true;
-        }
-
-        if (!element.checkValidity()) {
-            valid = false;
-            break;
-        }
-
-        if (element.value === "") {
-            valid = false;
-            break;
-        }
+    if (element.name === "model" && element.checked) {
+      checkedBox = true;
     }
 
-    if (valid && checkedBox) {
-      evaluateButton.disabled = false
+    if (!element.checkValidity()) {
+      valid = false;
+      break;
     }
-    else {
-        evaluateButton.disabled = true
+
+    if (element.value === "") {
+      valid = false;
+      break;
     }
-  };
+  }
+
+  if (valid && checkedBox) {
+    evaluateButton.disabled = false;
+  } else {
+    evaluateButton.disabled = true;
+  }
+};
 
 const selectAll = async () => {
-    document.getElementById("selectAll").addEventListener("change", (event) => {
-        const checkboxes = document.querySelectorAll(
-            'input[type="checkbox"][name="model"]'
-        );
-        for (let i = 0; i < checkboxes.length; i++) {
-            checkboxes[i].checked = event.target.checked;
-        }
-    });
-}
+  document.getElementById("selectAll").addEventListener("change", (event) => {
+    const checkboxes = document.querySelectorAll(
+      'input[type="checkbox"][name="model"]'
+    );
+    for (let i = 0; i < checkboxes.length; i++) {
+      checkboxes[i].checked = event.target.checked;
+    }
+  });
+};
 
 const evaluateButton = async () => {
-    document
+  document
     .getElementById("evaluate_button")
     .addEventListener("click", async (event) => {
       event.preventDefault();
       await plotModels();
     });
-}
+};
--- web/static/scripts/toggleComparisonInputs.js
+++ web/static/scripts/toggleComparisonInputs.js
@@ -1,20 +1,21 @@
-function hideK() {
-    const kInput = document.getElementById("comparison_inputs");
-    kInput.style.display = "none";
+const hideK = () => {
+  const kInput = document.getElementById("comparison_inputs");
+  kInput.style.display = "none";
 }
 
-function displayK() {
-    const kInput = document.getElementById("comparison_inputs");
-    kInput.style.display = "block";
+const displayK = () => {
+  const kInput = document.getElementById("comparison_inputs");
+  kInput.style.display = "block";
 }
 
-const toggleK = async() => {
-    document.getElementById('comparison_radio_group').addEventListener('change', (event) => {
-        if( event.target.id === 'precision_recall_f1') {
-            displayK();
-        }
-        else {
-            hideK();
-        }
+const toggleK = async () => {
+  document
+    .getElementById("comparison_radio_group")
+    .addEventListener("change", (event) => {
+      if (event.target.id === "precision_recall_f1") {
+        displayK();
+      } else {
+        hideK();
+      }
     });
-}
+};
